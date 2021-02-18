8fde5ad50fc1a38753e509add8b613262fdfd1fa
Lisa Mittmann <li.mittmann@gmail.com>
2021-02-18 16:30:16 +0100

    Add user db class and addUser
    Add user DB class including methods addUser and findUser
    addUser method will ignore duplicates
    Also add missing test for connector to test empty optional

--- backend/src/test/java/de/neuefische/githubbingomaster/api/GithubConnectorTest.java
+++ backend/src/test/java/de/neuefische/githubbingomaster/api/GithubConnectorTest.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +36,23 @@ class GithubConnectorTest {
 
         // Then
         assertThat(testUser.get(), is(new GithubUser("magnus", "http://awesomepics.com/magnus")));
+
+    }
+
+    @Test
+    public void getUserShouldReturnEmptyOptionalForNonExistingUser(){
+        // Given
+        String name = "magnus";
+        RestTemplate mockRestTemplate = mock(RestTemplate.class);
+        GithubConnector githubConnector = new GithubConnector(mockRestTemplate);
+
+        when(mockRestTemplate.getForEntity(url + "/users/" + name, GithubUser.class)).thenReturn(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+
+        //When
+        Optional<GithubUser> testUser = githubConnector.getUser(name);
+
+        //Then
+        assertTrue(testUser.isEmpty());
 
     }
 
