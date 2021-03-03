e69c6037b70fa886e08123b6c95e29a3c28243be
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-03-03 14:52:17 +0100

    Change password encoding to BCryptPasswordEncoder

--- backend/src/main/java/de/neuefische/githubbingomaster/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/githubbingomaster/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Override
