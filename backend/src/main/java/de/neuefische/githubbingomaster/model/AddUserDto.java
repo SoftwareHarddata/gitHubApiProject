f2fe92838bd9291b19c1d116aea2fd30288ccf85
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-03-05 11:41:11 +0100

    Remove unused import

--- backend/src/main/java/de/neuefische/githubbingomaster/model/AddUserDto.java
+++ backend/src/main/java/de/neuefische/githubbingomaster/model/AddUserDto.java
@@ -1,6 +1,5 @@
 package de.neuefische.githubbingomaster.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
